e50456bc56b4d7c2709e4adbe898aef66142de67
sahiltiwari <sahiltiwaricontact9@gmail.com>
2025-10-01 11:56:01 +0530

    doneenod

--- src/App.tsx
+++ src/App.tsx
@@ -27,6 +27,7 @@ import Regularization from "./pages/Regularization";
 import Payroll from "./pages/Payroll";
 import SalarySlips from "./pages/SalarySlips";  
 import Reports from "./pages/Reports";
+import EmployeesReport from "./pages/EmployeesReport";
 import CreateOrganization from "./pages/CreateOrganization";
 import OrganizationDetails from "./pages/OrganizationDetails";
 import EmployeeAttendanceDetail from "./pages/EmployeeAttendanceDetail";
@@ -68,6 +69,7 @@ const App = () => (
                       <Route path="/leaves/allotment" element={<LeaveAllotment />} />
                       <Route path="/leaves/allotment/history/:employeeId" element={<LeaveAllotmentHistory />} />
                       <Route path="/reports" element={<Reports />} />
+                      <Route path="/reports/employees" element={<EmployeesReport />} />
                       <Route path="/settings" element={<Settings />} />
                   <Route path="/profile" element={<Profile />} />
                       <Route path="/admin" element={<AdminPanel />} />
